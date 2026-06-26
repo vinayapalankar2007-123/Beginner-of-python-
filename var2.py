5f8b88f1bf4ce0c2f7f2c8fe2b2627ebf51d3c3b
vinayapalankar2007-123 <vinayapalankar2007@gmail.com>
2026-06-26 16:23:53 +0530

    Delete var2.py

--- var2.py
+++
@@ -1,6 +0,0 @@
-item = input("enter an item:  ")
-price = int(input("enter price: "))
-quantity = int(input("how much u want: "))
-
-total = price*quantity
-print(f"your total bill is {total} rupees!")
