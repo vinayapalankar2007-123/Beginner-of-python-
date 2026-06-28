80cabc1521aa32b10493652a0c6d40f0636c8a05
vinayapalankar2007-123 <vinayapalankar2007@gmail.com>
2026-06-28 15:06:55 +0530

    Add files via upload

---
+++ formatespecifiers.py
@@ -0,0 +1,7 @@
+#formate specifiers = {value:flags}
+x=3.1323324
+y=-6789.75890
+z=12.222
+print(f"x is {x:.2f}")
+print(f"y is {y:.3f}")
+print(f" z is {z:.1f}")
---
+++ indexing.py
@@ -0,0 +1,5 @@
+    #indexing= accessing elements of a seqence using [ ]
+                   #        [ start: end :step]
+ph_number ="123-233-32330"
+
+print(ph_number[0::2])
